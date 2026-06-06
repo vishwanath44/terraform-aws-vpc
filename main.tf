e5a7fd8491e8b72abb90da3de338bcd7907b7e28
vishwanath44 <chinthavishwanath9@gmail.com>
2026-06-06 15:56:40 +0530

    terraform-aws-vpc

--- main.tf
+++ main.tf
@@ -11,3 +11,14 @@ resource "aws_internet_gateway" "main" {
 
   tags = local.igw_final_tags
 }
+
+# Public Subnets
+/* resource "aws_subnet" "public" {
+  count  = length(var.public_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "var.public_subnet_cidrs[count.index]"
+
+  tags = {
+    Name = "Main"
+  }
+} */
--- variables.tf
+++ variables.tf
@@ -20,3 +20,8 @@ variable "igw_tags" {
     type = map
     default = {}
 }
+
+variable "public_subnet_cidrs" {
+    type = list
+    default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
