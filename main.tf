ad1e6558d19ca711aa3516bd82d6b5966560760c
vishwanath44 <chinthavishwanath9@gmail.com>
2026-06-07 12:22:22 +0530

    terraform-aws-vpc

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "aws_internet_gateway" "main" {
 resource "aws_subnet" "public" {
   count  = length(var.public_subnet_cidrs)
   vpc_id     = aws_vpc.main.id
-  cidr_block = "var.public_subnet_cidrs[count.index]"
+  cidr_block = var.public_subnet_cidrs[count.index]
   availability_zone = local.az_names[count.index]
   map_public_ip_on_launch = true
 
