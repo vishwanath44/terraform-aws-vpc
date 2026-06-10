cfcf8eb0b95353ecfcb54d1c7458b8fb57c63aa9
vishwanath44 <chinthavishwanath9@gmail.com>
2026-06-10 23:36:35 +0530

    terraform-aws-vpc

---
+++ peering.tf
@@ -0,0 +1,55 @@
+resource "aws_vpc_peering_connection" "default" {
+  count = var.is_peering_required ? 1 : 0
+  #peer_owner_id = var.peer_owner_id
+
+  # Acceptor
+  peer_vpc_id   = data.aws_vpc.default.id
+
+  # Requestor
+  vpc_id        = aws_vpc.main.id
+
+  auto_accept = true
+
+  accepter {
+    allow_remote_vpc_dns_resolution = true
+  }
+
+  requester {
+    allow_remote_vpc_dns_resolution = true
+  }
+
+  tags = merge(
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}-default"
+    }
+  )
+}
+
+resource "aws_route" "public_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id  = aws_vpc_peering_connection.default[count.index].id
+}
+
+resource "aws_route" "private_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id  = aws_vpc_peering_connection.default[count.index].id
+}
+
+resource "aws_route" "database_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.database.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id  = aws_vpc_peering_connection.default[count.index].id
+}
+
+resource "aws_route" "default_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = data.aws_route_table.default.id
+  destination_cidr_block    = var.vpc_cidr
+  vpc_peering_connection_id  = aws_vpc_peering_connection.default[count.index].id
+}
--- variables.tf
+++ variables.tf
@@ -79,3 +79,8 @@ variable "nat_gateway_tags" {
     default = {}
     type = map
 }
+
+variable "is_peering_required" {
+    default = false
+    type = bool
+}
